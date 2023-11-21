efaa2e3723340a8eb331ba53ef05d1f903583031
Hassan <derkaouihassan19@gmail.com>
2023-11-21 12:50:17 +0100

    create many components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeViewComponent } from './home-view/home-view.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductShowComponent } from './product-show/product-show.component';
+import { CartComponent } from './cart/cart.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { FooterComponent } from './footer/footer.component';
+import { UserShowComponent } from './user-show/user-show.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
+import { CategoryListComponent } from './category-list/category-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeViewComponent,
+    ProductListComponent,
+    ProductShowComponent,
+    CartComponent,
+    HeaderComponent,
+    LoginComponent,
+    RegistrationComponent,
+    FooterComponent,
+    UserShowComponent,
+    UserEditComponent,
+    CategoryListComponent
   ],
   imports: [
     BrowserModule
